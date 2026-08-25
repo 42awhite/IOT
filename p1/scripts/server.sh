a6bee1e6ccce965919bdc6b9e9276d3226663032
cronopi <robertoclases098@gmail.com>
2026-08-25 13:09:52 +0200

    Fix K3s token generation

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -2,7 +2,7 @@
 # 1. Instalación del servidor K3s (sin token hardcodeado)
 curl -sfL https://get.k3s.io | sh -s - server \
   --node-ip="192.168.56.110" \
-  --flannel-iface="eth1" \
+  --flannel-iface="eth1"
 
 # 2. Exportar el token generado dinámicamente a la carpeta compartida
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
